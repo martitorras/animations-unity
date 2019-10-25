63553e3a3eb9046235e5d1f56205de2357bb0c03
Martí Torras Isanta <martiti@TR12.CAT>
2019-10-25 11:56:37 +0200

    first

---
+++ anim/Assets/Animations/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    Animator m_Animator;
+    NavMeshAgent navmesh;
+    bool is_moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(navmesh.remainingDistance >= 0.1f)
+        {
+            m_Animator.SetBool("Is_moving", true);
+        }
+        else m_Animator.SetBool("Is_moving", false);
+
+
+
+    }
+}
